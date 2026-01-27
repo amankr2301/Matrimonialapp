d1f4480753e3566f0199abe4e2f0457ad5bd0f40
Aman Kumar <kraman2301@gmail.com>
2026-01-27 18:52:01 +0530

    clientcommit

--- API/Program.cs
+++ API/Program.cs
@@ -14,9 +14,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 });
 
+builder.Services.AddCors() ; 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200" , "http://localhost:4200")
+) ; 
 
 
 
